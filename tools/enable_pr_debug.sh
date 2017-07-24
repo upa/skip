f8c324f29ad092b09567d5098973a61d288a26bb
Ryo Nakamura <upa@haeena.net>
2017-07-24 22:32:14 +0900

    rm tools/enable_pr_debug.sh (moved to test)

--- tools/enable_pr_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# enable pr_debug()
-af_skip=`cd ../kmod && pwd`/af_skip.c
-skip_lwt=`cd ../kmod && pwd`/skip_lwt.c
-debugctl="/sys/kernel/debug/dynamic_debug/control"
-echo -n "file $af_skip  +p"  > $debugctl
-echo -n "file $skip_lwt  +p" > $debugctl
